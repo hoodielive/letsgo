f49ad4999622a7ba881b103fe36c62cc5fd4ee35
hoodielive <hoodielive@gmail.com>
2019-12-03 19:50:07 -0500

    Added a 404 page

--- web-fundamentals/ibeji.org/main.go
+++ web-fundamentals/ibeji.org/main.go
@@ -11,6 +11,9 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "<h1>Welcome to Ibeji</h1>")
 	} else if r.URL.Path == "/contact" {
 		fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:support@ibeji.org\">support@ibeji.org</a>.")
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w, "<h1>We could not find the page you were looking for: please send us an email.")
 	}
 }
 
