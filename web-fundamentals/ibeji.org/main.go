8fc188c2795496f9b96abf18de5ae8833d07e773
hoodielive <hoodielive@gmail.com>
2019-12-03 19:30:52 -0500

    Added a contact page

--- web-fundamentals/ibeji.org/main.go
+++ web-fundamentals/ibeji.org/main.go
@@ -7,7 +7,11 @@ import (
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>Welcome to the Site</h1>")
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>Welcome to Ibeji</h1>")
+	} else if r.URL.Path == "/contact" {
+		fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:support@ibeji.org\">support@ibeji.org</a>.")
+	}
 }
 
 func main() {
