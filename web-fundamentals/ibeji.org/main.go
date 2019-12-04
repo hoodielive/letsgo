85841ef2e4eef7451247dc509f40610deeae6a29
hoodielive <hoodielive@gmail.com>
2019-12-04 18:21:14 -0500

    Gorilla Mux handler explored

--- web-fundamentals/ibeji.org/main.go
+++ web-fundamentals/ibeji.org/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"github.com/julienschmidt/httprouter"
+	"github.com/gorilla/mux"
 	"net/http"
 )
 
@@ -18,13 +18,8 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
-}
 func main() {
-	router := httprouter.New()
-	router.GET("/hello/:name", Hello)
-
-	http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":3000", router)
+	r := mux.NewRouter()
+	r.HandleFunc("/", handlerFunc)
+	http.ListenAndServe(":3000", r)
 }
