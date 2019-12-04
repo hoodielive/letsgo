1c8f39074ffd7bddae8d019638a0e2a86f66bce8
hoodielive <hoodielive@gmail.com>
2019-12-04 17:49:09 -0500

    the reason, why I would never use the default server mux

--- web-fundamentals/ibeji.org/main.go
+++ web-fundamentals/ibeji.org/main.go
@@ -18,6 +18,7 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":3000", nil)
+	mux := &http.ServeMux{}
+	mux.HandleFunc("/", handlerFunc)
+	http.ListenAndServe(":3000", mux)
 }
