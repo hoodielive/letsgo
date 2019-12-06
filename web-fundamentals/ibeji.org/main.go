52b83fcfd07047103610075adff0b48a1d23d841
hoodielive <hoodielive@gmail.com>
2019-12-05 19:31:42 -0500

    Exercising

--- web-fundamentals/ibeji.org/main.go
+++ web-fundamentals/ibeji.org/main.go
@@ -16,9 +16,15 @@ func contact(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:support@ibeji.org\">support@ibeji.org</a>.")
 }
 
+func faq(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "text/html")
+	fmt.Fprint(w, "This is a faq page.")
+}
+
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/faq", faq)
 	http.ListenAndServe(":3000", r)
 }
