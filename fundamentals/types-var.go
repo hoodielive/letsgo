187f9fbffe2c795d2563dab855363ca33b52f6ea
hoodielive <hoodielive@gmail.com>
2019-12-08 12:00:26 -0500

    These are variable types

--- fundamentals/types-var.go
+++ fundamentals/types-var.go
@@ -12,4 +12,14 @@ func main() {
 	fmt.Printf("var b string \t %T [%v]\n", b, b)
 	fmt.Printf("var c float64 \t %T [%v]\n", c, c)
 	fmt.Printf("var d bool \t %T [%v]\n", d, d)
+
+	aa := 10
+	bb := "hello"
+	cc := 3.14159
+	dd := true
+
+	fmt.Printf("aa is \t %T [%v]\n", aa, aa)
+	fmt.Printf("bb is \t %T [%v]\n", bb, bb)
+	fmt.Printf("cc is \t %T [%v]\n", cc, cc)
+	fmt.Printf("dd is \t %T [%v]\n", dd, dd)
 }
