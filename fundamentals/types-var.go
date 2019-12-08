c041e09eb43aaf5f4407df614022446ec0926ab6
hoodielive <hoodielive@gmail.com>
2019-12-08 11:56:39 -0500

    These are variable types

---
+++ fundamentals/types-var.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int
+	var b string
+	var c float64
+	var d bool
+
+	fmt.Printf("var a int \t %T [%v]\n", a, a)
+	fmt.Printf("var b string \t %T [%v]\n", b, b)
+	fmt.Printf("var c float64 \t %T [%v]\n", c, c)
+	fmt.Printf("var d bool \t %T [%v]\n", d, d)
+}
