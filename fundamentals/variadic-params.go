2b44d2c4ea3c1a12d8f981c14a95e7aaa8c2d95c
hoodielive <hoodielive@gmail.com>
2019-12-12 18:40:33 -0500

    man go is so refreshing..

--- fundamentals/variadic-params.go
+++ fundamentals/variadic-params.go
@@ -26,5 +26,36 @@ func learnErrorHandling() {
 	if _, err := strconv.Atoi("non-int"); err != nil {
 		fmt.Println(err)
 	}
+	learnConcurrency()
+  }
+
+  func inc(i int , c chan int) {
+	  c <- i + 1
+  }
+
+  func learnConcurrency() {
+	  c := make(chan int)
+
+	  go inc(0, c)
+	  go inc(10, c)
+	  go inc(-805, c)
+
+	  fmt.Println(<-c, <-c, <-c)
+
+	  cs := make(chan string)
+	  ccs := make(chan chan string)
+	  go func() { c <- 84 }() 
+	  go func() { c <- 'wordy' }()
+
+	  select {
+	  case i := <-c:
+		fmt.Printf("It's a %T", i)
+	  case <-cs:
+		fmt.Println("It's a string")
+	  case <-ccs:
+		fmt.Println("Didnt happen..")
+	  }
+
+	  learnWebProgramming()
   }
 }
