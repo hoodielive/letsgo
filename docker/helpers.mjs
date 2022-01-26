81dc65f9c72591908dbbc1f414614cf50b295091
hoodielive <hoodielive@gmail.com>
2022-01-26 11:07:02 -0500

    Docker...

---
+++ docker/helpers.mjs
@@ -0,0 +1,14 @@
+const connectToDatabase = () =>
+{
+    const dummyPromise = new Promise((resolve, reject) =>
+    {
+        setTimeout(() => 
+        {
+            resolve();    
+        }, 1000);
+    });
+
+    return dummyPromise;
+};
+
+export default connectToDatabase;
